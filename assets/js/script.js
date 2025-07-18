91e4bbe748338573b72fc4121d273063539ca3b1
SimoneCriscuolo233 <simone.criscuolo26052001@gmail.com>
2025-07-18 13:50:57 +0200

    feat: create a function to contain the logic of email generation and !  added a button that generates 10 new emails and replaces the old ones

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,20 @@ const list = document.getElementById("mail-list")
 const button = document.getElementById("submit-button")
 
 const mail = [];
-
-for (let i = 0; i < 10; i++) {
-  axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`).then((res) => {
-    mail.push(res.data.response)
-    list.innerHTML += `<li class="list-group-item">${mail[i]}</li>`
-  })
-
+const generateMail = () => {
+  for (let i = 0; i < 10; i++) {
+    axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`).then((res) => {
+      mail.push(res.data.response)
+      list.innerHTML += `<li class="list-group-item">${mail[i]}</li>`
+    })
+  }
 }
+
+
+button.addEventListener('click', () => {
+  list.innerHTML = ``;
+  generateMail()
+})
+
+
+generateMail()
