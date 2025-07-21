adf6c23292de634d8096c55995465128de448a89
SimoneCriscuolo233 <simone.criscuolo26052001@gmail.com>
2025-07-21 09:37:23 +0200

    minFix: Simplify the logic and HTML for the loading spinner display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,10 @@
 //https://flynn.boolean.careers/exercises/api/random/mail
 const list = document.getElementById("mail-list")
 const button = document.getElementById("submit-button")
-const background = document.getElementById('spinner-bg')
-const spinner= document.getElementById('spinner')
+const spinner = document.getElementById('spinner')
 list.classList.add('d-none')
 const mail = [];
 const generateMail = (min) => {
-  background.classList.remove('d-none')
   spinner.classList.remove('d-none')
   for (let i = 0; i < min; i++) {
     axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`).then((res) => {
@@ -14,14 +12,13 @@ const generateMail = (min) => {
       list.innerHTML += `<li class="list-group-item">${mail[i]}</li>`
       if (mail.length === min - 1) {
         list.classList.remove("d-none")
-        background.classList.add('d-none')
         spinner.classList.add('d-none')
       }
     })
   }
 }
-        
-        
+
+
 
 
 button.addEventListener('click', () => {
